d1d252fb4e9254b4bca48a9206b40f5cbc144b40
Julia812-r <sotilejulia07@gmail.com>
2025-08-06 14:48:31 -0300

    Update app_verdes.py

--- app_verdes.py
+++ app_verdes.py
@@ -249,9 +249,8 @@ elif menu_opcao == "Registros de Empréstimos":
 
         # Garante que colunas texto sejam strings e datas formatadas em string
         colunas_texto = [
-            "Nome Completo do Solicitante", "Email do Solicitante", "IPN do Solicitante", "Departamento", "Telefone", "Local e motivo da utilização", "Nome Completo do Supervisor", "Email do Supervisor",
-            "SV Veículo", "Placa", "Pernoite", "Projeto", "Data Registro", "Previsão de Devolução" 
-        ]
+            "Nome Solicitante", "Email Solicitante", "Departamento", "IPN Solicitante", "Telefone", "CNH", "Validade CNH", "Nome Supervisor", "Email Supervisor",
+            "Motivo", "GoodCard", "SV Veículo", "Placa", "Pernoite", "Projeto", "Data Registro"
 
         for col in colunas_texto:
             if col in df_exibicao.columns:
@@ -313,6 +312,7 @@ elif menu_opcao == "Registros de Empréstimos":
 
             salvar_dados(df_editavel)
             
+
 
 
 
